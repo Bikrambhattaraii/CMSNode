e5fe422db90541fe71319658848d58e4a0adaff8
bikram bhattarai <bikrambhattarai296@gmail.com>
2023-09-09 17:26:48 +0545

    er

--- app.js
+++ app.js
@@ -1,68 +1,71 @@
 const express= require("express")
 const { blogs } = require("./model/index")
-const { EDGE_RUNTIME_WEBPACK } = require("next/dist/shared/lib/constants")
+
 const app =express()
 // telling node js to set view engine
-app.set('view engine','ejs')
-app.use(express.json) // form bata li nodejs lai vanna lai
-// from bata airacha parse gar or handle gar vancxah yo code le
-
 // database connection 
-require('./model/index')
+require("./model/index")
+
+// telling the nodejs to set view-engine to ejs
+app.set('view engine','ejs')
+
+
+// form bata data aairaxa parse gara or handle gar vaneko ho
+app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-// allblogs
-app.get('/', async(req,res)=>{
-    // table bata data nikalnu paryo
-     const allBlogs = await blogs.findAll()  //mongodb .find()  // blogs ko table ko data allBlogs ma store
-     console.log(allBlogs)
-    res.render('blogs',{blogs:allBlogs}) // blogs ma ako data all blogs ma pass garne {blogs:allBlogs} le ejs file lai  
-    // blogs.ejs halda ni huncah
+
+// allBlog
+app.get("/",async (req,res)=>{
+    //blogs vanney table bata vayejati sabai data dey vaneko 
+    const allBlogs = await blogs.findAll() 
+
+    // blogs vanney key/name ma allBlogs/data pass gareko ejs file lai
+    res.render('blogs',{blogs:allBlogs})
 })
+
 //createBlog
-app.get('/createBlog',(req,res)=>{
-    res.render('createBlog')
-})
-// single blog
-app.get('/singleBlog:id',async(req,res)=>{
-    const id =req.params.id
-    // 
-    // const{id}=req.params
-    console.log(req.params.id)
-    res.render('singleBlog')
-
-    // id ko data magne orfind garne table bata
-  await  blogs.findAll({
-        where:{
-            id:id
-        }
-    })
-  
-
-    console.log(blog)
-    res.render('singleBlogs',{blog:blog} )
+app.get("/createBlog",(req,res)=>{
+    res.render("createBlog")
 })
 
-//createBlog post 
+//createBlog Post
 app.post("/createBlog",async (req,res)=>{
-    // const title =req.body.title 
-    // const description =req.body.description
-    // const subTitle= req.body.subTitle
-    // desctructuring previous 
- const{title,subTitle,description }=req.body
- console.log(title.description.subTitle)
-//database ma halni
-// database ma input puraucha
- await blogs.create({  // kei time lagcha db ma halna nikalna so await use garne
-    title: title,
-    subTitle:subTitle,
-    description:description,  
- })  
- 
-    //req.body.title or description ni halna milcha 
-    res.redirect("/") 
+    
+        // second approach
+        // const {title,description,subtitle} = req.body
+    // first approach
+    const title = req.body.title
+    const description  = req.body.description
+    const subTitle = req.body.subtitle
+   
+    // database ma halnu paryo , database sanaga kehi operation await halnu parney hunchha 
+    // agadi , await halepaxi mathi async halnu parney hunchha 
+    await blogs.create({
+        title : title,
+        subTitle:subTitle,
+        description : description
+    })
+    res.redirect("/")
 })
-// find() database bata tanne
-//createBlog set 
+
+// single blog page 
+app.get("/single/:id",async(req,res)=>{
+const id = req.params.id
+// second approach
+// const {id} = req.params 
+// id ko data magnu/find garnu paryo hamro table bata
+const blog =  await blogs.findAll({
+    where : {
+        id : id
+    }
+})
+// second finding approach
+// const blog = await blogs.findByPk(id)
+
+    res.render("singleBlog",{blog:blog})
+})
+
+
 app.listen(3000,()=>{
-    console.log("Node Js project ")
+    console.log("NodeJs project has started at port 3000")
 })
